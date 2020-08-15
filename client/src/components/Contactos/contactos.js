77596833222dbbd5db5c1375ac39f988595f5ee3
German Cejas <germancejas.1988@gmail.com>
2020-08-14 23:11:26 -0300

    contactos responsive

--- client/src/components/Contactos/contactos.js
+++ client/src/components/Contactos/contactos.js
@@ -6,11 +6,12 @@ import {
   addContact,
 } from "../../actions/contactsActions";
 import { SELECT_CONTACT } from "../../constants/userConstants";
-import henry from "../Usuario/images/henry.svg";
 import "./contactos.css";
 import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import { getProfile } from "../../actions/UserActions";
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
 
 const Contacts = () => {
   const dispatch = useDispatch();
@@ -42,38 +43,11 @@ const Contacts = () => {
  }
 
   return (
-    <div id="onboarding">
-      <div class="titulo">
-        <img class="logo" src={henry} alt="logo"></img>
-        <h1 class="title">Contactos</h1>
-      </div>
-      <div>
-        <svg
-          id="svg1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="#000000"
-            fill-opacity="1"
-            d="M0,256L80,256C160,256,320,256,480,218.7C640,181,800,107,960,106.7C1120,107,1280,181,1360,218.7L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-          ></path>
-        </svg>
-        <svg
-          id="svg2"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="#fffe00"
-            fill-opacity="1"
-            d="M0,288L80,277.3C160,267,320,245,480,240C640,235,800,245,960,213.3C1120,181,1280,107,1360,69.3L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
-          ></path>
-        </svg>
-      </div>
-      <div id="container" class="row justify-content-center">
+    <Container id="contactoscont">
+      <Image id="logomiscontactos" src="https://fotos.subefotos.com/42c7f4c4e2f06d48681a4a5094ca38b5o.png" ></Image>
+      <div id="contactoscont2" class="row justify-content-center">
         <div class="col-auto">
-          <Table size="lg" borderless="true" hover="true">
+          <Table id="contactostable"size="lg" borderless="true" hover="true">
             <thead>
               <tr>
                 <th>Nombre</th>
@@ -108,48 +82,60 @@ const Contacts = () => {
         ></input>
 
         {userSelected !== "" ? (
-          <div>
+          <div id="buttonscont">
             <Button
               className="btn btn-dark"
+              id="addbtn"
               variant="top"
               size="lg"
               onClick={() => addHandler()}
             >
-              Agregar
+              Agregar Contacto
             </Button>
             <Button
+              id="elimbtn"
               className="btn btn-dark"
               variant="top"
               size="lg"
               onClick={() => deleteHandler(userSelected.email, userContected.id)}
             >
-              Eliminar
+              Eliminar Contacto
             </Button>
           </div>
         ) : (
-          <div>
+          <div id="buttonscont">
             <Button
               onClick={() => addHandler(contacts.email)}
+              id="addbtn"
               className="btn btn-dark"
               variant="top"
               size="lg"
             >
-              Agregar
+              Agregar Contacto
             </Button>
-            <Button disabled className="btn btn-dark" variant="top" size="lg">
-              Eliminar
+            <Button 
+            id="elimbtn"
+            disabled className="btn btn-dark" 
+            variant="top" 
+            size="lg">
+              Eliminar Contacto
             </Button>
                         
           </div>
         )}
       </div>
-      <div className="VolverDin"> <Button
-              onClick={volver}
-              className="btn btn-dark"
-              variant="top"
-              size="lg"
-            > Volver a Enviar Dinero</Button></div>
-    </div>
+      <div className="volvercont"> 
+        <Button
+          onClick={volver}
+          id="volverbutton"
+          className="btn btn-dark"
+          variant="top"
+          size="lg"
+        > Volver a Enviar Dinero
+        </Button>
+      </div>
+      <Image id="footercontactos" src="https://fotos.subefotos.com/0d5c65b0be7d80bce6ee2187e71c9997o.png" ></Image>
+    </Container>
   );
 };
 
